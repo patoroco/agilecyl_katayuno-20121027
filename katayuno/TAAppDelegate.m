c33578482e721402d43c05533dc7cddc99b73fb7
Jorge Maroto <patoroco@gmail.com>
2012-10-27 10:42:26 +0200

    First Commit

--- katayuno/TAAppDelegate.m
+++ katayuno/TAAppDelegate.m
@@ -8,23 +8,18 @@
 
 #import "TAAppDelegate.h"
 
-#import "TAViewController.h"
 
 @implementation TAAppDelegate
 
 - (void)dealloc
 {
     [_window release];
-    [_viewController release];
     [super dealloc];
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
-    // Override point for customization after application launch.
-    self.viewController = [[[TAViewController alloc] initWithNibName:@"TAViewController" bundle:nil] autorelease];
-    self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
 }
